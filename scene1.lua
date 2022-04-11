6a6579fbc725ad0cdf570c7ad353e485b47fd707
Cheng Cao <bobcaocheng@gmail.com>
2022-04-10 22:11:12 -0700

    thing

--- scene1.lua
+++ scene1.lua
@@ -15,16 +15,24 @@ local scene = {
             max_hp = 50,
             atk = 9,
             def = 0,
-            atk_cd = 3.5,
-            move_cd = 2.5,
+            atk_cd = 5.5,
+            move_cd = 3.5,
         }
     },
     initial_pieces = {
         friendly = {
-            { type = "A", x = 5, y = 5 }
+            { type = "A", x = 5, y = 2 },
+            { type = "A", x = 6, y = 2 },
+            { type = "A", x = 7, y = 2 },
+            { type = "A", x = 8, y = 2 },
+            { type = "A", x = 9, y = 2 },
         },
         enemy = {
-            { type = "A", x = 3, y = 3 }
+            { type = "A", x = 5, y = 9 },
+            { type = "A", x = 6, y = 9 },
+            { type = "A", x = 7, y = 9 },
+            { type = "A", x = 8, y = 9 },
+            { type = "A", x = 9, y = 9 },
         }
     }
 }
